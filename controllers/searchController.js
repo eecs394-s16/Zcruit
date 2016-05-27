eea423487bee0fb403883ce6786db2c56ef482a8
paep3nguin <paep3nguin@gmail.com>
2016-05-26 22:02:28 -0500

    Visits and better table formatting

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -4,6 +4,7 @@ angular.module('zcruit').controller('searchController', ['$scope', '$location', 
   var coach = 1;
   $scope.sortParam = 'FirstName';
   $scope.sortReverse = false;
+  $scope._ = _;
 
   // Called when an option is selected from the lists drop-down
   $scope.showList = function() {
