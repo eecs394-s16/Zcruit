9cd1be01a0eded4e2cc2d879d3de024177c04cb7
paep3nguin <paep3nguin@gmail.com>
2016-05-30 19:44:39 -0500

    Drag and drop works, updates in DB

--- controllers/bigBoardController.js
+++ controllers/bigBoardController.js
@@ -20,6 +20,20 @@ angular.module('zcruit').controller('bigBoardController', ['$scope','$location',
     }
     return "bright-red";
   };
+
+  // Run an arbitrary query, callback is passed the response if the query succeeds
+  function runQuery(queryString, callback) {
+    $http.get('https://zcruit-bpeynetti.c9users.io/php/query.php?query=' + encodeURIComponent(queryString))
+    .then(function(response) {
+      if (response.status === 200) {
+        if (callback) {
+          callback(response.data);
+        }
+      } else {
+        console.log("Query error: " + response);
+      }
+    });
+  }
   
   // Begin List JS
   $scope.showLists = false;
@@ -33,8 +47,7 @@ angular.module('zcruit').controller('bigBoardController', ['$scope','$location',
 
   $http.get('https://zcruit-bpeynetti.c9users.io/php/query.php?query='+ encodeURIComponent('SELECT DISTINCT List_name FROM SavedLists'))
   .then(function(response){
-    $scope.myLists = eval(response.data);
-    console.log($scope.myLists);
+    $scope.myLists = response.data;
   });
 
   // End List JS
@@ -50,14 +63,43 @@ angular.module('zcruit').controller('bigBoardController', ['$scope','$location',
     }
   };
 
-  $http.get('https://zcruit-bpeynetti.c9users.io/php/query.php?query='+ encodeURIComponent('SELECT DISTINCT Position_name FROM Positions'))
-  .then(function(response){
-  	$scope.positions = eval(response.data);
-  	console.log($scope.positions);
+  $http.get('https://zcruit-bpeynetti.c9users.io/php/query.php?query=' + encodeURIComponent('SELECT DISTINCT * FROM Players p, HighSchools h, Positions pos WHERE p.HighSchool_id = h.HS_id AND p.Player_id = pos.Player_id ORDER BY Position_name, Position_rank'))
+  .then(function(response) {
+    // Put the players into their positions
+    // Players are already ordered by position rank, so no sorting needed
+    var players = response.data;
+    var positions = {};
+    for (var i = 0, l = players.length; i < l; i++) {
+      var p = players[i];
+      var pos = p.Position_name;
+      if (pos in positions) {
+        positions[pos].push(p);
+      } else {
+        positions[pos] = [p];
+      }
+    }
+
+    $scope.positions = positions;
   });
 
-  $http.get('https://zcruit-bpeynetti.c9users.io/php/query.php?query=' + encodeURIComponent('SELECT DISTINCT * FROM Players p, HighSchools h, Positions pos WHERE p.HighSchool_id = h.HS_id AND p.Player_id = pos.Player_id'))
-  .then(function(response) {
-    $scope.players = eval(response.data);
-  });
+  $scope.reorder = function(event, pos, newIndex, oldIndex) {
+    $scope.positions[pos].splice(newIndex, 0, $scope.positions[pos].splice(oldIndex, 1)[0]);
+
+    // Update the position ranks in the database
+    var query = "UPDATE Positions SET Position_rank = CASE Pos_id";
+    var lo = Math.min(newIndex, oldIndex);
+    var hi = Math.max(newIndex, oldIndex);
+    var ids = [];
+    for (var i = lo; i <= hi; i++) {
+      var p = $scope.positions[pos][i];
+      ids.push(p.Pos_id);
+      query += " WHEN " + p.Pos_id + " THEN " + (i + 1);
+    }
+    query += " END WHERE Pos_id IN (" + ids.join(',') + ")";
+    runQuery(query);
+  };
+
+  $scope.noSwipe = function(event) {
+    event.preventDefault();
+  };
 }]);
